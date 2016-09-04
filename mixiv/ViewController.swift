38a749b93c7ca0907356b74d7407a0be35a70604
AKIO <maizima44ww@gmail.com>
2016-09-04 13:12:35 +0900

    Add new project file.

---
+++ mixiv/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mixiv
+//
+//  Created by 板谷晃良 on 2016/09/04.
+//  Copyright © 2016年 AkkeyLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
