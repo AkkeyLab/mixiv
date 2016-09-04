ca43fdd89ab89fcb582b646cdaa73e7b66427125
AKIO <maizima44ww@gmail.com>
2016-09-04 19:13:05 +0900

    仮置きHOMEデザイン（広告部分）

---
+++ mixiv/AdvertisementView.swift
@@ -0,0 +1,21 @@
+//
+//  AdvertisementView.swift
+//  mixiv
+//
+//  Created by 板谷晃良 on 2016/09/04.
+//  Copyright © 2016年 AkkeyLab. All rights reserved.
+//
+
+import UIKit
+
+class AdvertisementView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
